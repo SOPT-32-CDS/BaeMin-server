f9cef0e35681423aee0c0e4f46886fae0f888fcc
chaerin <achrvvv@gmail.com>
2023-05-16 21:02:38 +0900

    feat: implement jwt token logic

--- src/main/java/sopt/org/cds/service/UserService.java
+++ src/main/java/sopt/org/cds/service/UserService.java
@@ -3,6 +3,7 @@ package sopt.org.cds.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Base64Utils;
 import sopt.org.cds.controller.user.dto.UserResponseDto;
 import sopt.org.cds.domain.User;
 import sopt.org.cds.infrastructure.UserRepository;
@@ -16,8 +17,16 @@ public class UserService {
     private final UserRepository userRepository;
 
     @Transactional
-    public UserResponseDto getUserInfo(Long id) {
-        Optional<User> user = userRepository.findById(id);
+    public UserResponseDto authentication(String token) {
+        String[] split = token.split(" ");
+        String credential = split[1];
+
+        String decoded = new String(Base64Utils.decodeFromString(credential));
+
+        String[] userIdAndAddress = decoded.split(":");
+        Long userId = Long.parseLong(userIdAndAddress[0]);
+
+        Optional<User> user = userRepository.findById(userId);
 
         if (user.isPresent()) {
             return UserResponseDto.of(user.get().getAddress());
