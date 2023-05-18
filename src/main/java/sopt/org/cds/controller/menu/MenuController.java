5b5a0e1fb34d2dc35627a2c0ef8be6b5c0957120
chaerin <achrvvv@gmail.com>
2023-05-18 20:08:18 +0900

    feat: add controller and service

--- src/main/java/sopt/org/cds/controller/menu/MenuController.java
+++ src/main/java/sopt/org/cds/controller/menu/MenuController.java
@@ -2,6 +2,10 @@ package sopt.org.cds.controller.menu;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import sopt.org.cds.service.MenuService;
 
@@ -9,5 +13,12 @@ import sopt.org.cds.service.MenuService;
 @RequiredArgsConstructor
 public class MenuController {
     private final MenuService menuService;
+
+    @GetMapping("menu/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Long getMenuDetail(@PathVariable final Long id) {
+        
+        return id;
+    }
 
 }
--- src/main/java/sopt/org/cds/service/MenuService.java
+++ src/main/java/sopt/org/cds/service/MenuService.java
@@ -2,10 +2,16 @@ package sopt.org.cds.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import sopt.org.cds.controller.menu.dto.MenuResponseDto;
 import sopt.org.cds.infrastructure.MenuRepository;
 
 @Service
 @RequiredArgsConstructor
 public class MenuService {
     private final MenuRepository menuRepository;
+
+    public MenuResponseDto getMenuDetail() {
+        MenuResponseDto response = new MenuResponseDto();
+        return response;
+    }
 }
