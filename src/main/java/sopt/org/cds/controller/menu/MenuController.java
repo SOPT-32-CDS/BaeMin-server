a28c6f49fd6b478a56099f91a8a8b4590edfb120
chaerin <achrvvv@gmail.com>
2023-05-18 20:38:04 +0900

    feat: implement menu detail api

--- src/main/java/sopt/org/cds/controller/menu/MenuController.java
+++ src/main/java/sopt/org/cds/controller/menu/MenuController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import sopt.org.cds.common.dto.ApiResponseDto;
+import sopt.org.cds.controller.menu.dto.MenuDetailResponseDto;
+import sopt.org.cds.exception.SuccessStatus;
 import sopt.org.cds.service.MenuService;
 
 @RestController
@@ -16,9 +19,9 @@ public class MenuController {
 
     @GetMapping("menu/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public Long getMenuDetail(@PathVariable final Long id) {
-        
-        return id;
+    public ApiResponseDto<MenuDetailResponseDto> getMenuDetail(@PathVariable final Long id) {
+        MenuDetailResponseDto data = menuService.getMenuDetail(id);
+        return ApiResponseDto.success(SuccessStatus.GET_MENU_DETAIL_SUCCESS, data);
     }
 
 }
