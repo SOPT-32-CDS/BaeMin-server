c776158764d73ae695373b064c490c295bac8af8
chaerin <achrvvv@gmail.com>
2023-05-22 20:25:21 +0900

    fix: change has coupon to coupon

---
+++ src/test/java/sopt/org/cds/service/UserServiceTest.java
@@ -0,0 +1,14 @@
+package sopt.org.cds.service;
+
+import org.junit.jupiter.api.Test;
+
+class UserServiceTest {
+
+    @Test
+    void authentication() {
+    }
+
+    @Test
+    void getUserIdWithToken() {
+    }
+}
